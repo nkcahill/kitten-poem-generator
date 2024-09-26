86c8996ddf89cc5773817200b2869dd4e2bfae03
nkcahill <67979424+nkcahill@users.noreply.github.com>
2024-09-26 10:48:26 -0400

    poem generating

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ function showResponse(response) {
     });
   }
   
-  function getPoem() {
+  function getPoem(event) {
+    event.preventDefault();
+    console.log("Generating poem");
     let apiKey = "b041fca7a906td7b8234b9f8d05bo382";
-    let context = "provide a short poem with a kitten or kittens as the subject and include the topic in the prompt";
-    let prompt = "Tell me a poem about kittens";
+    let context = "Provide a 4 line poem and separate each line with a <br />. The poem should be about a kitten or kittens as the subject and include the topic in the prompt. ";
+    let topic = document.querySelector("#topic");
+    let prompt = `Tell me a poem about kittens and ${topic.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     axios.get(apiUrl).then(showResponse);
   }
